d5a29ffc7b2eb618f3114bbe3b05ec01e0a01330
Dan Denysovets <dan.denysovets@bndigital.co>
2022-04-21 20:51:35 +0300

    feat(app): campaigns page created

---
+++ src/pages/campaigns/index.tsx
@@ -0,0 +1,14 @@
+import{ FC} from 'react'
+import { MainLayout } from '../../components/layout/mainLayout'
+
+
+const Campaigns: FC = () => {
+
+    return (
+        <MainLayout>
+            <h1>Campaigns Page should be there</h1>
+        </MainLayout>
+    )
+}
+
+export { Campaigns as default }
