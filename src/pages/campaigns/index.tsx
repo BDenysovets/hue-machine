9dbd1736d24a276360463e7b96d7b35138c6090e
Dan Denysovets <dan.denysovets@bndigital.co>
2022-04-23 17:08:49 +0300

    feat(app): campaign page condition render added on auth

--- src/pages/campaigns/index.tsx
+++ src/pages/campaigns/index.tsx
@@ -1,14 +1,18 @@
-import{ FC} from 'react'
+import{ FC } from 'react'
 import { MainLayout } from '../../components/layout/mainLayout'
+import {useAuth} from "../../contexts/Auth";
+import {Login} from "./Login";
+import {List} from "./List";
 
 
 const Campaigns: FC = () => {
+  const { token } = useAuth()
 
-    return (
-        <MainLayout>
-            <h1>Campaigns Page should be there</h1>
-        </MainLayout>
-    )
+  return (
+    <MainLayout>
+      {token ? <List /> : <Login />}
+    </MainLayout>
+  )
 }
 
 export { Campaigns as default }
