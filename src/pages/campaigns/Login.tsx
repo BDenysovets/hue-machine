9f17d23fa440ad62264cca86eb98885aa5b2b552
Dan Denysovets <dan.denysovets@bndigital.co>
2022-04-23 22:30:46 +0300

    feat(app): signin form updated

--- src/pages/campaigns/Login.tsx
+++ src/pages/campaigns/Login.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import { GoogleLoginResponse, GoogleLogin } from 'react-google-login';
 import {useAuth} from "../../contexts/Auth";
+import {Stack, Typography} from "@mui/material";
 
 const Login: FC = () => {
   const { setToken, user: { setUser } } = useAuth()
@@ -12,13 +13,23 @@ const Login: FC = () => {
   }
 
   return (
-    <GoogleLogin
-      clientId={clientId}
-      buttonText="Login"
-      onSuccess={handleResponse}
-      onFailure={handleResponse}
-      cookiePolicy={'single_host_origin'}
-    />
+    <Stack
+      justifyContent="center"
+      alignItems="center"
+      gap={2}
+      sx={{
+        height: 500
+      }}
+    >
+      <Typography variant={'h4'} align={'center'} >Please login to proceed</Typography>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={handleResponse}
+        onFailure={handleResponse}
+        cookiePolicy={'single_host_origin'}
+      />
+    </Stack>
   )
 }
 
