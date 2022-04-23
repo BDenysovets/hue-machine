567969b1ba2354c9d336a3db63f1cbe73f5abad2
Dan Denysovets <dan.denysovets@bndigital.co>
2022-04-23 20:53:21 +0300

    feat(app): auth logout function added

--- src/contexts/Auth.tsx
+++ src/contexts/Auth.tsx
@@ -16,6 +16,7 @@ type UserContextProps = {
   user: { data: UserT | null, setUser: (data: UserT) => void }
   authenticated: boolean
   setToken: (token: string) => void
+  logout: () => void
 }
 
 const defaultValue: UserContextProps = {
@@ -23,6 +24,7 @@ const defaultValue: UserContextProps = {
   user: { data: null, setUser: () => undefined },
   authenticated: false,
   setToken: () => undefined,
+  logout: () => undefined
 }
 
 const UserContext = createContext<UserContextProps>(defaultValue)
@@ -32,10 +34,15 @@ const UserProvider: FC = ({ children }) => {
   const [user, setUser] = useState<UserT>()
   const [authenticated, setAuthenticated] = useState(false)
 
+  function logout() {
+    setUser(null)
+    setToken(null)
+  }
+
   useEffect(() => setAuthenticated(!!token && !!user), [user, token])
 
   return (
-    <UserContext.Provider value={{ token, setToken, authenticated, user: { data: user, setUser } }}>
+    <UserContext.Provider value={{ token, setToken, authenticated, logout, user: { data: user, setUser } }}>
       {children}
     </UserContext.Provider>
   )
