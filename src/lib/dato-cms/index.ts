d77213be603de26cc7db76627abce4ab96d22ffb
BDenysovets <bdenysovets@gmail.com>
2022-05-26 22:56:10 +0300

    fix(app): datocms API added

---
+++ src/lib/dato-cms/index.ts
@@ -0,0 +1,32 @@
+import {buildClient} from "@datocms/cma-client";
+
+export const apiToken = process.env.NEXT_PUBLIC_DATO_CMS_API_KEY;
+export const cmsClient = buildClient({
+  apiToken: apiToken,
+  environment: 'campaign-manager-dev',
+});
+
+export const modelsId = {
+  partner: '180521',
+  asset: '180527',
+  campaign: '227995',
+};
+
+export async function create(data: Record<any, any>, id: keyof typeof modelsId): Promise<any> {
+  return await cmsClient.items.create({
+    item_type: { type: 'item_type', id },
+    ...data,
+  });
+}
+
+export async function update(data: Record<any, any>, id: string): Promise<any> {
+  return await cmsClient.items.update(id, {...data});
+}
+
+export async function findOne(id: string) {
+  return await cmsClient.items.find(id, {version: 'published'});
+}
+
+export async function find(id: keyof typeof modelsId): Promise<any> {
+  return await cmsClient.items.list({filter: {type: modelsId[id]}});
+}
