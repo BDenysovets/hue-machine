cade0e048d6b2bc26e832c5abaf1bc1fb3631f77
BDenysovets <bdenysovets@gmail.com>
2022-05-27 14:52:16 +0300

    feat(app): props renamed

--- src/pages/campaigns/index.tsx
+++ src/pages/campaigns/index.tsx
@@ -3,14 +3,14 @@ import {Table} from '../../components/pages/campaigns/Table';
 import {ContractT, getAllContracts} from '../../lib/nft-port';
 
 export async function getStaticProps() {
-  const campaigns = await getAllContracts('rinkeby')
+  const contracts = await getAllContracts('rinkeby')
 
   return {
-    props: { data: campaigns },
+    props: { contracts },
     revalidate: 1
   };
 }
 
-const Campaigns: FC<{ data: Array<ContractT> }> = ({ data }) => <Table campaigns={data} />
+const Campaigns: FC<{ contracts: Array<ContractT> }> = ({ contracts }) => <Table contracts={contracts} />
 
 export { Campaigns as default };
