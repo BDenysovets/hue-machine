a5b3c91021809d4ef18e9f6960aa7fe30d05baaa
BDenysovets <bdenysovets@gmail.com>
2022-10-14 13:00:40 +0300

    feat: 404 page added

--- src/components/typography/Title.tsx
+++ src/components/typography/Title.tsx
@@ -1,13 +1,31 @@
 import {PropsWithChildren} from "react";
+import './Title.scss';
+import cx from "classnames";
 
 type Props = {
-  level?: 1 | 2 | 3 | 4 | 5 | 6
+  level: 1 | 2 | 3 | 4 | 5 | 6;
+  className?: string;
+  upperCase?: boolean;
+
 }
 
-const Title = ({ children, level }: PropsWithChildren<Props>) => {
-  return (
-    <div>{children}</div>
-  )
+const Title = ({ children, level = 1, className, upperCase }: PropsWithChildren<Props>) => {
+  const setClassNames = (level: number) => cx(className, `typography-title-${level}`, upperCase && upperCase);
+
+  switch (level) {
+    case 1:
+      return <h1 className={setClassNames(1)}>{children}</h1>;
+    case 2:
+      return <h2 className={setClassNames(2)}>{children}</h2>;
+    case 3:
+      return <h3 className={setClassNames(3)}>{children}</h3>;
+    case 4:
+      return <h4 className={setClassNames(4)}>{children}</h4>;
+    case 5:
+      return <h5 className={setClassNames(5)}>{children}</h5>;
+    case 6:
+      return <h6 className={setClassNames(6)}>{children}</h6>;
+  }
 }
 
 export { Title }
--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -4,13 +4,14 @@ import balloon from './assets/balloon.svg'
 import {CTAButton} from "../../components/ctaButton/CTAButton";
 import './index.scss'
 import {Content} from "../../components/layout/Content";
+import {Title} from "../../components/typography/Title";
 
 const NotFound: FC = () => {
   return (
     <DefaultLayout hasFooter={false} className="notFoundPage">
       <Content className="notFound">
         <div className="visual">
-          <h1 className="title">404</h1>
+          <Title className="title" level={1}>404</Title>
           <img className="image" src={balloon} alt="balloon"/>
         </div>
         <div className="actions">
