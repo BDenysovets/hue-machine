e749a89d5a1da99289e2de1bc3002a714da2ddfc
BDenysovets <bdenysovets@gmail.com>
2022-10-16 23:33:31 +0300

    feat: header wip

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,6 +6,7 @@ import {AppTheme} from "../../types/types";
 import {Link, useLocation} from "react-router-dom";
 import {useEffect, useMemo, useState} from "react";
 import cx from "classnames";
+import {useMenuContext} from "../../contexts/MenuContext";
 
 type Props = {
   hasBurger: boolean
@@ -14,6 +15,7 @@ type Props = {
 
 const Header = ({ hasBurger, theme }: Props) => {
   const elementsTheme = theme === 'light' ? 'dark' : 'light'
+  const { isOpen, isMenuRunning } = useMenuContext();
   const { pathname } = useLocation();
   const hasHeaderLink = useMemo(() => !pathname.includes('contacts'),[pathname])
   const [isGetInTouchVisible, setIsGetInTouchVisible] = useState(false);
@@ -31,9 +33,11 @@ const Header = ({ hasBurger, theme }: Props) => {
   return (
     <div className='header'>
       <div className="wrapper">
-        <Logo theme={elementsTheme} />
+        <div style={{ transition: isOpen && isMenuRunning ? 'opacity 0.3s' : 'initial', opacity: !isOpen ? 1 : 0, visibility: !isOpen && !isMenuRunning ? 'visible' : 'hidden' }}>
+          <Logo theme={elementsTheme} />
+        </div>
         <div className="headerActions">
-          {hasHeaderLink && <Link to="/contacts" className={cx(elementsTheme, isGetInTouchVisible && 'visible')}>Get in touch</Link>}
+          {hasHeaderLink && !isOpen && !isMenuRunning && <Link to="/contacts" className={cx(elementsTheme, isGetInTouchVisible && 'visible')}>Get in touch</Link>}
           {hasBurger ? <Burger theme={elementsTheme} /> : <div />}
         </div>
       </div>
