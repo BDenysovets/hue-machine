44296934f897071b8557d83bad981ef9cc969bc1
BDenysovets <bdenysovets@gmail.com>
2022-10-16 23:48:52 +0300

    feat: menu container added

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,6 +1,8 @@
 import './Menu.scss'
 import {useMenuContext} from "../../contexts/MenuContext";
 import cx from "classnames";
+import {useLocation} from "react-router-dom";
+import {Content} from "../layout/Content";
 
 const Cover = () => {
   const { isMenuRunning } = useMenuContext();
@@ -10,11 +12,52 @@ const Cover = () => {
   )
 }
 
+type MenuItemType = {
+  link: string;
+  title: string;
+  value?: string;
+}
+
+const menuItems: Array<MenuItemType> = [
+  {
+    link: '/',
+    title: 'Index',
+    value: ''
+  },
+  {
+    link: '/work',
+    title: 'Work',
+    value: '139'
+  },
+  {
+    link: '/about',
+    title: 'About',
+  },
+  {
+    link: '/join',
+    title: 'Join',
+    value: '3'
+  },
+  {
+    link: '/contacts',
+    title: 'Contact',
+  },
+]
+
 const MenuContent = () => {
   const { isMenuRunning, isOpen } = useMenuContext();
+  const { pathname } = useLocation()
 
   return (
-    <div className={cx('menu', { 'open': isOpen, 'closing': !isOpen && isMenuRunning })}></div>
+    <div className={cx('menu', { 'open': isOpen, 'closing': !isOpen && isMenuRunning })}>
+      <Content className="menuContainer">
+        <div className="menuWrapper">
+          <div className="menuInner">
+
+          </div>
+        </div>
+      </Content>
+    </div>
   )
 }
 
