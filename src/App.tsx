2404bf57aade238995e72d66dbb47e955d897b2c
BDenysovets <bdenysovets@gmail.com>
2022-10-17 18:04:31 +0300

    feat: join page added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import NotFound from "./pages/not-found";
 import Home from "./pages/home";
 import Contacts from "./pages/contacts";
+import Join from "./pages/join";
 
 import './styles/main.scss'
 
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route index element={<Home />} />
       <Route path='/contacts' element={<Contacts />} />
+      <Route path="/join" element={<Join />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
---
+++ src/pages/join/index.tsx
@@ -0,0 +1,63 @@
+import {FC} from "react";
+import { DefaultLayout } from "../../components/layout/DefaultLayout";
+import {Content} from "../../components/layout/Content";
+import {Title} from "../../components/typography/Title";
+import './index.scss'
+
+const jobs = {
+  design: [
+    {
+      title: 'Product Designer Intern',
+      description: 'We help companies to grow their relationships with their customers, solve problems, embrace exciting opportunities in different domains',
+    },
+    {
+      title: 'Middle Product Designer',
+      description: 'We help companies to grow their relationships with their customers, solve problems, embrace exciting opportunities in different domains',
+    },
+    {
+      title: 'Middle Product Designer',
+      description: 'We help companies to grow their relationships with their customers, solve problems, embrace exciting opportunities in different domains',
+    },
+  ],
+  development: [
+    {
+      title: 'Senior Developer',
+      description: 'We help companies to grow their relationships with their customers, solve problems, embrace exciting opportunities in different domains',
+    },
+  ],
+  'marketing & communications': [
+    {
+      title: 'Marketing Executive',
+      description: 'We help companies to grow their relationships with their customers, solve problems, embrace exciting opportunities in different domains',
+    },
+  ],
+}
+
+const Join: FC = () => {
+  return (
+    <DefaultLayout theme="dark">
+      <Content className="joinPage">
+        <Title level={2} className="joinPageTitle">Join</Title>
+        <div className="joinListWrapper">
+          <div className="joinListInner">
+            {Object.entries(jobs).map(([domain, jobs]) => (
+              <div className="domainItem">
+                <p className="jobDomainTitle">{domain}</p>
+                <div className="domainJobs">
+                  {jobs.map(job => (
+                    <div className="domainJobItem">
+                      <Title level={5}>{job.title}</Title>
+                      <p className="jobDescription">{job.description}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Content>
+    </DefaultLayout>
+  )
+}
+
+export default Join
