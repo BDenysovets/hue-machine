823b86453d487416cbedfd2a13379c1cf71253a7
BDenysovets <bdenysovets@gmail.com>
2022-10-23 00:45:42 +0300

    feat: texts updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {
   TransitionGroup,
   CSSTransition
 } from "react-transition-group";
+// import { Cursor } from 'react-creative-cursor'
 
 import NotFound from "./pages/not-found";
 import Home from "./pages/home";
@@ -19,23 +20,26 @@ function App() {
   const { setCoverRunning } = useMenuContext();
 
   return (
-    <TransitionGroup>
-      <CSSTransition
-        key={pathname}
-        classNames="page"
-        timeout={1400}
-        onEnter={() => setCoverRunning()}
-      >
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path='/contacts' exact component={Contacts} />
-          <Route path="/join" exact component={Join} />
-          <Route path="/work" exact component={Work} />
-          <Route path="/about" exact component={About} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </CSSTransition>
-    </TransitionGroup>
+    <>
+      <TransitionGroup>
+        <CSSTransition
+          key={pathname}
+          classNames="page"
+          timeout={1400}
+          onEnter={() => setCoverRunning()}
+        >
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path='/contacts' exact component={Contacts} />
+            <Route path="/join" exact component={Join} />
+            <Route path="/work" exact component={Work} />
+            <Route path="/about" exact component={About} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+      {/*<Cursor isGelly={true} cursorSize={20} cursorBackgrounColor="#fff" exclusionBackgroundColor="#eee" />*/}
+    </>
   );
 }
 
--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,16 +3,19 @@ import cx from "classnames";
 
 import {AppTheme} from "../../types/types";
 import './Logo.scss'
+import CursorLink from '../../assets/CursorLink.svg'
 
 type Props = {
   theme?: AppTheme
 }
 
 const Logo = ({ theme = 'dark' }: Props) => (
-  <Link to='/' className={cx('logo', theme)}>
-    <span>hue&</span>
-    <span>machine</span>
-  </Link>
+  <div data-cursor-background-image={CursorLink} data-cursor-size="200px">
+    <Link to='/' className={cx('logo', theme)}>
+      <span>hue&</span>
+      <span>machine</span>
+    </Link>
+  </div>
 )
 
 export { Logo }
